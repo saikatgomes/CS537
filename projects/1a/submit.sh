3fd7902630d5e74db5776d3644510e21273813ea
saikatgomes <saikatgomes@gmail.com>
2015-02-07 23:11:23 -0600

    clean up

---
+++ projects/1a/submit.sh
@@ -0,0 +1,5 @@
+echo "copying multable ..."
+cp -f multable/* ~cs537-2/handin/saikat/p1/linux/multable/
+echo "copying tnine ..."
+cp -f tnine/* ~cs537-2/handin/saikat/p1/linux/tnine/
+echo "files copied"
